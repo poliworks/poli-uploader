8d07608853acd2e7f5d80477a07a88c7ba828b9f
Leonardo Iacovini <leoiacovini@gmail.com>
2016-12-12 01:12:09 -0200

    Upload endpoint

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject poli-uploader "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [clj-aws-s3 "0.3.10"]
+                 [ring/ring-json "0.4.0"]
+                 [digest "1.4.5"]
+                 [ring/ring-defaults "0.2.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler poli-uploader.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
---
+++ src/poli_uploader/handler.clj
@@ -0,0 +1,31 @@
+(ns poli-uploader.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.multipart-params :as mp]
+            [aws.sdk.s3 :as s3]
+            [clojure.string :as str]
+            [digest]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
+
+(def aws-cred {:access-key (System/getenv "AWS_ACCESS_KEY")
+               :secret-key (System/getenv "AWS_SECRET_KEY")})
+
+(defn upload-object [info file]
+  (let [key (str (:type info) "/" (digest/digest "md5" file) "." (:extension info))]
+    (s3/put-object aws-cred (:bucket info) key file {:content-type (str "image/" (:extension info))})
+    (str "https://s3-sa-east-1.amazonaws.com/" (:bucket info) "/" key)))
+
+(defn handle-upload [{:keys [params] :as request}]
+  (println params)
+  {:status 200 :body {:uri (upload-object (assoc params :extension (-> (get params "file") :filename (str/split #"\.") last)) (->> (get params "file") :tempfile))}})
+
+(defroutes app-routes
+  (POST "/upload/:bucket/:type" request (handle-upload request))
+  (route/not-found "Not Found"))
+
+(def app
+  (-> app-routes
+      (mp/wrap-multipart-params)
+      (wrap-json-response)
+      (wrap-defaults api-defaults)))
